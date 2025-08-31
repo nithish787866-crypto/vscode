10cfcce64ee56a60a383d17a52b96198d0bdb1e1
Nithish <nithish787866@gmail.com>
2025-08-30 20:56:54 -0600

    two

---
+++ pro1.py
@@ -0,0 +1,24 @@
+class percentage:
+    name =""
+    age =""
+    tamil =""
+    englidh =""
+    maths =""
+    def add(self):
+      a=int("name")+int("englidh")+int("maths")
+      print(a) 
+
+    
+Nithish=percentage()
+Nithish.name ="nithish"
+Nithish.age ="20"
+Nithish.tamil="100"
+Nithish.englidh ="50"
+Nithish.maths ="70"
+print (Nithish.name)
+print (Nithish.age)
+print (Nithish.tamil)
+print (Nithish.englidh)
+print (Nithish.maths)
+
+Nithish.add()
